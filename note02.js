e5f40e8bf855dbaf757e31b6ec96c310ca1e8a60
hasibferdous <hasibferdous.ict@gmail.com>
2022-08-04 15:36:19 +0600

    notes

--- note02.js
+++ note02.js
@@ -1,5 +1,7 @@
 /**
+ * --------------------------------------------------------------------------------
  * ARRAY
+ * --------------------------------------------------------------------------------
  * 1. What is the purpose of array?
  * 2. How to declare an array in JS?
  * 3. Number of elements in an array
@@ -12,4 +14,104 @@
  * 10. How can you remove the last element from array
  * 11. Add an element at the first position of an array
  * 12. Remove the first element of an array 
+ * --------------------------------------------------------------------------------
  */
+
+
+//1. What is the purpose of array?
+ /**
+  * In JavaScript, array is a single variable that is used to store different elements. It is often used when we 
+  * want to store list of elements and access them by a single variable. Unlike most languages where array is a 
+  * reference to the multiple variable, in JavaScript array is a single variable that stores multiple elements.
+    
+    Declaration of an Array 
+    There are basically two ways to declare an array. 
+    Example: 
+            var House = [ ]; // method 1 
+            var House = new Array(); // method 2 
+        But generally method 1 is preferred over the method 2. Let us understand the reason for this. 
+  */
+
+
+//2. How to declare an array in JS?
+ /**
+  * Declaration of an Array 
+    There are basically two ways to declare an array. 
+    Example: 
+            var House = [ ]; // method 1 
+            var House = new Array(); // method 2 
+        But generally method 1 is preferred over the method 2. Let us understand the reason for this. 
+  */
+
+
+//3. Number of elements in an array
+ /**
+  * var variableName = [elements];
+  */
+
+
+// 4. What is index?
+ /**
+  * The 'index' is the postion of the values in an array.
+  * 
+  * The indexOf() method returns the first index (position) of a specified value.
+
+    The indexOf() method returns -1 if the value is not found.
+
+    The indexOf() method starts at a specified index and searches from left to right.
+
+    By default the search starts at the first element and ends at the last.
+
+    Negative start values counts from the last element (but still searches from left to right).
+  */
+
+
+//5. Find the value of an element by index
+ /**
+  * The indexOf() method returns the position of a specified value or element.
+  */
+  var fruits = [ 'apple', 'banana', 'orange', 'watermelon'];
+  var index = fruits.indexOf('watermelon')
+    console.log(index);
+
+
+//6. Change an element by index
+ /**
+  * 
+  */
+
+
+//7. Get the index of an element by the value
+ /**
+  * 
+  */
+
+
+//8. What does it mean when get undefined while getting the value of an element
+ /**
+  * 
+  */
+
+
+//9. How can you add an element to an array at the last positon
+ /**
+  * 
+  */
+ 
+
+//10. How can you remove the last element from array
+ /**
+  * 
+  */
+
+
+//11. Add an element at the first position of an array
+ /**
+  * 
+  */
+
+
+//12. Remove the first element of an array
+ /**
+  * 
+  */  
